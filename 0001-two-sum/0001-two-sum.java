438a20e9795264fd9d2467976c20b4a989826952
Farhan Khan <farhan.work435@gmail.com>
2024-12-22 12:12:35 -0500

    Time: 46 ms (19.25%), Space: 44.9 MB (44.87%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+
+        if(nums.length < 2){
+            return nums;
+        }
+
+        int indices[] = new int[2];
+
+        for(int i = 0; i < nums.length; i++){
+
+            for(int j = i+1; j < nums.length; j++){
+
+                if(nums[i] + nums[j] == target){
+                    indices[0] = i;
+                    indices[1] = j;
+                    return indices;
+                }
+            }
+        }
+
+        return nums;
+    }
+}
