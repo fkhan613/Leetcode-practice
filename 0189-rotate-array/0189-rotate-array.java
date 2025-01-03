0c38ca185939b1070d404796c9b5773cd176e677
Farhan Khan <farhan.work435@gmail.com>
2025-01-02 19:35:42 -0500

    Time: 8 ms (5.02%), Space: 55.5 MB (97.98%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        int temp;
+
+        for(int i = 0; i < nums.length; i++){
+            deque.add(nums[i]);
+        }
+        
+        for(int j = 1; j <= k; j++){
+            temp = deque.removeLast();
+            deque.addFirst(temp);
+        }
+
+        int index = 0;
+        for(int val : deque){
+            nums[index++] = val;
+        }
+    }
+}
