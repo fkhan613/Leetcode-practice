d566881a5d699bb4f9474c4a70c67667419b4483
Farhan Khan <farhan.work435@gmail.com>
2025-01-19 11:38:58 -0500

    Time: 0 ms (100%), Space: 42.2 MB (60%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,20 +1,11 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        String substr = "";
-        String result = "";
-
-        for (char i : str1.toCharArray()) {
-            String newstr1 = str1;
-            String newstr2 = str2;
-            substr += i;
-            if (str1.indexOf(substr) != -1) {
-                if (newstr1.replaceAll(substr, "").equals("") && newstr2.replaceAll(substr, "").equals("")) {
-                    result = substr;
-                }
-            } else {
-                return "";
-            }
-        }
-        return result;
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+        int gcd = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcd);
+    }
+    public int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
