83936ddaf4160cc74c6aa77d9984882276dcbc37
Farhan Khan <farhan.work435@gmail.com>
2025-02-11 09:02:14 -0500

    Time: 22 ms (8.34%), Space: 48.5 MB (99.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,40 +2,32 @@ class Solution {
     public int majorityElement(int[] nums) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
-        int currentMaxOccurences = 0;
-        int currentMaxNum = 0;
+        int currentMaxOcc = 0;
+        int currentMaxOccNum = 0;
 
-        /**
-            {
-                num : occurences 
-            }
-
-         */
-
-         if(nums.length <= 1){
-            return nums[0];
-         }
-
-
+        //loop through the array and count the number of occurences each element has
         for(int i = 0; i < nums.length; i++){
 
+            //check if the number has been seen, if so increment the number of occurences
             if(map.containsKey(nums[i])){
+                
                 int incrementedOcc = map.get(nums[i]);
                 incrementedOcc++;
                 map.put(nums[i], incrementedOcc);
-    
+
             } else{
                 map.put(nums[i], 1);
-                continue;
             }
 
-            if(map.get(nums[i]) > currentMaxOccurences){
-                currentMaxOccurences = map.get(nums[i]);
-                currentMaxNum = nums[i];
+            //keep track of the currentMaxOcc and number
+            if(map.get(nums[i]) > currentMaxOcc){
+                currentMaxOcc = map.get(nums[i]);
+                currentMaxOccNum = nums[i];
             }
 
         }
 
-        return currentMaxNum;
+        return currentMaxOccNum; 
+
     }
 }
