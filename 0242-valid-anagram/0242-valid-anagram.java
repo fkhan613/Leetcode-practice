88c6513d69b31e91a84b291bf1e6c9254d956e6b
Farhan Khan <farhan.work435@gmail.com>
2025-02-15 22:22:34 -0500

    Time: 20 ms (12.88%), Space: 44.6 MB (52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        //{character found in s : count}
+        HashMap<Character, Integer> sCharacters = new HashMap<Character, Integer>();
+
+        if(s.length() != t.length()) return false;
+
+        //loop through s and put the characters in the hashmap
+        for(int i = 0; i < s.length(); i++){
+
+            if(sCharacters.containsKey(s.charAt(i))){
+                int count = sCharacters.get(s.charAt(i));
+                ++count;
+                sCharacters.put(s.charAt(i), count);
+            } else{
+                sCharacters.put(s.charAt(i), 1);
+            }
+
+        }
+
+        //loop through t and see if you the word is a valid anagram
+        for(int i = 0; i < t.length(); i++){
+
+            //check if the current character is in the hashmap and has a count of >0
+            if(sCharacters.containsKey(t.charAt(i)) && sCharacters.get(t.charAt(i)) > 0){
+                //means we can use the character
+                int decCount = sCharacters.get(t.charAt(i));
+                --decCount;
+                sCharacters.put(t.charAt(i), decCount);
+            } else{
+                return false;
+            } 
+
+        }
+
+        return true;
+        
+    }
+}
